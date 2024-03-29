957e871d4ca5f614a80bdf7958efe4046bd84cd7
Huseyin-Celebi <hcelebi057@gmail.com>
2024-03-29 15:56:21 +0300

    solutions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import com.workintrch.burger.DeluxeBurger;
+import com.workintrch.burger.Hamburger;
+import com.workintrch.burger.HealthyBurger;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        Hamburger hamburger = new Hamburger("Basic", 3.56, "Wrap");
+
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+
+        hamburger.itemizeHamburger();
+
+        HealthyBurger healthyBurger = new HealthyBurger("Vegan Burger", 5.67, "Sandwich");
+
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHealthyAddition1("Lentils", 3.41);
+
+        healthyBurger.itemizeHamburger();
+
+        DeluxeBurger db = new DeluxeBurger();
+
+        db.addHamburgerAddition3("Should not do this", 50.53);
+
+        db.itemizeHamburger();
+
     }
 }
---
+++ src/com/workintrch/burger/DeluxeBurger.java
@@ -0,0 +1,29 @@
+package com.workintrch.burger;
+
+public class DeluxeBurger extends Hamburger{
+    public DeluxeBurger() {
+        super("Deluxe Burger", 19.10, "Double Sandwich");
+        addHamburgerAddition1("Chips", 2.75);
+        addHamburgerAddition2("Drink", 1.81);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Deluxe Burger için yeni malzeme eklenemez");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Deluxe Burger için yeni malzeme eklenemez");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Deluxe Burger için yeni malzeme eklenemez");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Deluxe Burger için yeni malzeme eklenemez");
+    }
+}
